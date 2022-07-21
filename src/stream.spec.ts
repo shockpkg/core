7bec198f5f3d768a61f1648295243c38e09d53fd
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2022-07-21 01:14:11 -0400

    Created StreamSlice to output only a slice of data from a stream

--- src/stream.spec.ts
+++ src/stream.spec.ts
@@ -3,7 +3,7 @@ import {join as pathJoin} from 'path';
 import {promisify} from 'util';
 import {rm, mkdir, lstat} from 'fs/promises';
 
-import {createWriterStream} from './stream';
+import {createWriterStream, StreamSlice} from './stream';
 
 const pipe = promisify(pipeline);
 
@@ -15,7 +15,11 @@ class Reader extends Readable {
 	// eslint-disable-next-line @typescript-eslint/naming-convention
 	public _read(size: number) {
 		for (let i = 0; i < 5; i++) {
-			this.push(Buffer.alloc(MB));
+			const b = Buffer.alloc(MB);
+			for (let i = 0; i < b.length; i++) {
+				b[i] = i % 256;
+			}
+			this.push(b);
 		}
 		this.push(null);
 	}
@@ -43,6 +47,153 @@ describe('stream', () => {
 			await pipe(reader, writer);
 			expect((await lstat(file)).size).toBe(5 * MB);
 			expect(wrotes).toEqual([MB, 2 * MB, 3 * MB, 4 * MB, 5 * MB]);
+		});
+	});
+
+	describe('StreamSlice', () => {
+		it('2MB - 10b', async () => {
+			const reader = new Reader();
+			const transform = new StreamSlice(2 * MB, 10);
+			const datas: number[][] = [];
+			transform.on('data', (data: Buffer) => {
+				datas.push([...data]);
+			});
+			await pipe(reader, transform);
+			expect(datas).toEqual([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]]);
+		});
+
+		it('2MB - 0b', async () => {
+			const reader = new Reader();
+			const transform = new StreamSlice(2 * MB, 0);
+			const datas: number[][] = [];
+			transform.on('data', (data: Buffer) => {
+				datas.push([...data]);
+			});
+			await pipe(reader, transform);
+			expect(datas).toEqual([]);
+		});
+
+		it('2MB+1 - 0b', async () => {
+			const reader = new Reader();
+			const transform = new StreamSlice(2 * MB + 1, 0);
+			const datas: number[][] = [];
+			transform.on('data', (data: Buffer) => {
+				datas.push([...data]);
+			});
+			await pipe(reader, transform);
+			expect(datas).toEqual([]);
+		});
+
+		it('2MB+1 - 10b', async () => {
+			const reader = new Reader();
+			const transform = new StreamSlice(2 * MB + 1, 10);
+			const datas: number[][] = [];
+			transform.on('data', (data: Buffer) => {
+				datas.push([...data]);
+			});
+			await pipe(reader, transform);
+			expect(datas).toEqual([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]);
+		});
+
+		it('2MB-1 - 10b', async () => {
+			const reader = new Reader();
+			const transform = new StreamSlice(2 * MB - 1, 10);
+			const datas: number[][] = [];
+			transform.on('data', (data: Buffer) => {
+				datas.push([...data]);
+			});
+			await pipe(reader, transform);
+			expect(datas).toEqual([[0xff], [0, 1, 2, 3, 4, 5, 6, 7, 8]]);
+		});
+
+		it('2MB-4 - 4b', async () => {
+			const reader = new Reader();
+			const transform = new StreamSlice(2 * MB - 4, 4);
+			const datas: number[][] = [];
+			transform.on('data', (data: Buffer) => {
+				datas.push([...data]);
+			});
+			await pipe(reader, transform);
+			expect(datas).toEqual([[0xfc, 0xfd, 0xfe, 0xff]]);
+		});
+
+		it('1.5MB - 2MB', async () => {
+			const reader = new Reader();
+			const transform = new StreamSlice(1.5 * MB, 2 * MB);
+			const datas: Buffer[] = [];
+			transform.on('data', (data: Buffer) => {
+				datas.push(data);
+			});
+			await pipe(reader, transform);
+			expect(datas.length).toBe(3);
+			expect(datas[0].length).toBe(0.5 * MB);
+			expect(datas[1].length).toBe(MB);
+			expect(datas[2].length).toBe(0.5 * MB);
+		});
+
+		it('4MB - -1', async () => {
+			const reader = new Reader();
+			const transform = new StreamSlice(4 * MB, -1);
+			const datas: Buffer[] = [];
+			transform.on('data', (data: Buffer) => {
+				datas.push(data);
+			});
+			await pipe(reader, transform);
+			expect(datas.length).toBe(1);
+			expect(datas[0].length).toBe(MB);
+		});
+
+		it('4MB-1 - -1', async () => {
+			const reader = new Reader();
+			const transform = new StreamSlice(4 * MB - 1, -1);
+			const datas: Buffer[] = [];
+			transform.on('data', (data: Buffer) => {
+				datas.push(data);
+			});
+			await pipe(reader, transform);
+			expect(datas.length).toBe(2);
+			expect(datas[0].length).toBe(1);
+			expect(datas[1].length).toBe(MB);
+		});
+
+		it('4MB+1 - -1', async () => {
+			const reader = new Reader();
+			const transform = new StreamSlice(4 * MB + 1, -1);
+			const datas: Buffer[] = [];
+			transform.on('data', (data: Buffer) => {
+				datas.push(data);
+			});
+			await pipe(reader, transform);
+			expect(datas.length).toBe(1);
+			expect(datas[0].length).toBe(MB - 1);
+		});
+
+		it('4.5MB - -1', async () => {
+			const reader = new Reader();
+			const transform = new StreamSlice(4.5 * MB, -1);
+			const datas: Buffer[] = [];
+			transform.on('data', (data: Buffer) => {
+				datas.push(data);
+			});
+			await pipe(reader, transform);
+			expect(datas.length).toBe(1);
+			expect(datas[0].length).toBe(0.5 * MB);
+		});
+
+		it('0.5MB - -1', async () => {
+			const reader = new Reader();
+			const transform = new StreamSlice(0.5 * MB, -1);
+			const datas: Buffer[] = [];
+			transform.on('data', (data: Buffer) => {
+				datas.push(data);
+			});
+			await pipe(reader, transform);
+			expect(datas.length).toBe(5);
+			expect(datas[0].length).toBe(0.5 * MB);
+			expect(datas[1].length).toBe(MB);
+			expect(datas[2].length).toBe(MB);
+			expect(datas[3].length).toBe(MB);
+			expect(datas[4].length).toBe(MB);
 		});
 	});
 });
--- src/stream.ts
+++ src/stream.ts
@@ -1,5 +1,6 @@
 /* eslint-disable max-classes-per-file */
 import {WriteStream, createWriteStream} from 'fs';
+import {Transform, TransformCallback} from 'stream';
 
 /**
  * Extends WriteStream.
@@ -77,4 +78,116 @@ export function createWriterStream(
 		path: Parameters<typeof createWriteStream>[0],
 		options: Parameters<typeof createWriteStream>[1]
 	) => WriterStream)(path, options);
+}
+
+/**
+ * Gets buffer slice out of a readable stream.
+ */
+export class StreamSlice extends Transform {
+	/**
+	 * Slice start.
+	 */
+	public readonly start: number;
+
+	/**
+	 * Slice size.
+	 */
+	public readonly size: number;
+
+	/**
+	 * Amount processed.
+	 */
+	protected _transformed = 0;
+
+	/**
+	 * StreamSlice constructor.
+	 *
+	 * @param start Start offset.
+	 * @param size Total Size.
+	 */
+	constructor(start = 0, size = -1) {
+		super();
+
+		this.start = start;
+		this.size = size;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	public _transform(
+		chunk: any,
+		encoding: BufferEncoding,
+		callback: TransformCallback
+	): void {
+		const {start, size} = this;
+		let c =
+			encoding && (encoding as string) !== 'buffer'
+				? Buffer.from(chunk as string, encoding)
+				: (chunk as Buffer);
+
+		// If size is 0, then nothing to do.
+		if (size === 0) {
+			this._transformed += c.length;
+			return callback();
+		}
+
+		// If size is less than 0, from start to infinity.
+		if (size < 0) {
+			// Find index from start.
+			const i = this._transformed - start;
+
+			// Discard if all before start.
+			if (i + c.length <= 0) {
+				this._transformed += c.length;
+				return callback();
+			}
+
+			// Skip over any data before start.
+			if (i < 0) {
+				this._transformed += -i;
+				c = c.subarray(-i);
+			}
+
+			this._transformed += c.length;
+			this.push(c.subarray());
+			return callback();
+		}
+
+		// Discard if all past end.
+		if (this._transformed >= start + size) {
+			this._transformed += c.length;
+			return callback();
+		}
+
+		// Find index from start.
+		let i = this._transformed - start;
+
+		// Discard if all before start.
+		if (i + c.length <= 0) {
+			this._transformed += c.length;
+			return callback();
+		}
+
+		// Skip over any data before start.
+		if (i < 0) {
+			this._transformed += -i;
+			c = c.subarray(-i);
+			i = 0;
+		}
+
+		// If check length more than remaining.
+		const r = size - i;
+		if (c.length > r) {
+			this._transformed += r;
+			this.push(c.subarray(0, r));
+			this._transformed += c.length - r;
+			return callback();
+		}
+
+		this._transformed += c.length;
+		this.push(c.subarray());
+		return callback();
+	}
 }
